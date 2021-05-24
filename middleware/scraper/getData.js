a8b0e113d22cbc4f8b61c078616c26c60947e798
Brian Heise <bnheise@gmail.com>
2021-05-25 08:47:05 +0900

    webscraper complete

--- middleware/scraper/getData.js
+++ middleware/scraper/getData.js
@@ -6,9 +6,14 @@ fetch('https://www.tokyodev.com/')
   .then(res => res.text())
   .then(body => {
     const parsedBody = parse(body);
-    const companyElements = parsedBody.querySelectorAll('.media-object-text > a');
-    //companyElements.forEach(element => console.log(element.rawAttrs))
-    const companyNames = companyElements.filter(element => element.rawAttrs.match(/href="\/companies\/\w+\/"\w?/))
-      .map(element => element.childNodes[0].rawText);
-    console.log(companyNames);
+    const jobsItems = parsedBody.querySelectorAll('.jobs-list-item').slice(1);
+    const results = jobsItems.map(item => {
+      const columns = item.childNodes.filter(column => column?.rawAttrs?.match(/jobs-list-column/));
+      const companyName = columns[0].childNodes.find(node => node?.rawAttrs?.match(/media-object-text/)).childNodes[0].childNodes[0]?.rawText
+      const jobTitle = columns[1]?.childNodes[1].childNodes[0]?.rawText
+      const tags = columns[2]?.childNodes[1].childNodes.filter(node => node?.rawAttrs?.match(/class="tag"/)).map(node2 => node2.childNodes[0]?.rawText)
+      return { name: companyName, title: jobTitle, tags }
+    })
+      .filter(item => item.name && item.title && item.tags)
+    console.log(results)
   });
